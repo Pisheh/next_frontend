14ee7b658455f64804da1e881a98ab544b09584d
Sepehr Gorgini <agent.1378@gmail.com>
2023-05-30 21:56:45 +0330

    Adjust font size

--- app/Footer/FooterLinks.tsx
+++ app/Footer/FooterLinks.tsx
@@ -10,9 +10,9 @@ interface FooterLinksProps {
 const FooterLinks: React.FC<FooterLinksProps> = ({ title, links }) => {
   return (
     <ul className='my-4 text-center lg:text-right lg:m-0'>
-      <h4 className='mb-4 font-extrabold lg:text-lg'>{title}</h4>
+      <h4 className='mb-4 font-bold xl:text-lg'>{title}</h4>
       {links?.map(link => (
-        <li className='my-3 text-sm' key={link.linkName}>
+        <li className='my-3 text-xs xl:text-sm' key={link.linkName}>
           <Link href={link.path} key={link.linkName}>
             {link.linkName}
           </Link>
