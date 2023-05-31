476e4860d418dd8effa8e9f877f7370c8135355a
Sepehr Gorgini <agent.1378@gmail.com>
2023-05-31 17:43:15 +0330

    Remove default width and height

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -6,9 +6,7 @@ const Card = ({
   className?: string
 }) => {
   return (
-    <div
-      className={`bg-white w-[85%] md:w-3/4 lg:w-[600px] lg:h-[600px] rounded-[40px] ${className} py-10 px-4 lg:px-0 lg:py-0`}
-    >
+    <div className={`bg-white ${className} py-10 px-4 lg:px-0 lg:py-0`}>
       {children}
     </div>
   )
