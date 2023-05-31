03a3e73546d66ec6f3ee5bec65360bebb9369008
Sepehr Gorgini <agent.1378@gmail.com>
2023-05-31 17:40:24 +0330

    Remove default drop shadow

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -7,7 +7,7 @@ const Card = ({
 }) => {
   return (
     <div
-      className={`bg-white w-[85%] md:w-3/4 lg:w-[600px] lg:h-[600px] rounded-[40px] shadow-xl ${className} py-10 px-4 lg:px-0 lg:py-0`}
+      className={`bg-white w-[85%] md:w-3/4 lg:w-[600px] lg:h-[600px] rounded-[40px] ${className} py-10 px-4 lg:px-0 lg:py-0`}
     >
       {children}
     </div>
