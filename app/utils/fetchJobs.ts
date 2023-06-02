2580f01857b5122b6c68931c39cd89716049cddc
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-03 00:51:40 +0330

    Change config to not cache data

--- app/utils/fetchJobs.ts
+++ app/utils/fetchJobs.ts
@@ -2,6 +2,7 @@ const fetchJobs = async () => {
   const res = await fetch('http://199.231.235.83:8923/jobs/page', {
     method: 'POST',
     mode: 'no-cors',
+    cache: 'no-store',
     headers: {
       'Content-Type': 'application/json'
     },
@@ -13,6 +14,5 @@ const fetchJobs = async () => {
 
   return res.json()
 }
-
 
 export default fetchJobs
