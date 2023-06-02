270a0c587a70e56d981a49d4186e849a95c285d2
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-03 00:03:58 +0330

    Add proper types

--- app/jobs/JobSkills.tsx
+++ app/jobs/JobSkills.tsx
@@ -2,6 +2,11 @@ import { HiCheckCircle } from 'react-icons/hi'
 import JobHeading from '../components/JobHeading'
 import { store } from '../store'
 
+interface skill {
+  id: number
+  title: string
+}
+
 const JobSkills = () => {
   const { skills } = store.getState().selectJob.job
 
@@ -9,7 +14,7 @@ const JobSkills = () => {
     <div>
       <JobHeading>مهارت ها و شاخص ها</JobHeading>
       <ul className='py-5 space-y-4'>
-        {skills.map(skill => (
+        {skills.map((skill: skill) => (
           <li key={skill.id} className='flex flex-row items-center gap-2'>
             <HiCheckCircle className='text-2xl text-gray-400' />
             <span>{skill.title}</span>
