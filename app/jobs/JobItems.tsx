6e571f808739ab3e1864e04f9cee96434d833e74
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-04 20:20:16 +0330

    Update component

--- app/jobs/JobItems.tsx
+++ app/jobs/JobItems.tsx
@@ -7,32 +7,27 @@ import JobItem from './JobItem'
 
 const JobItems = ({ jobs }: { jobs: any }) => {
   const dispatch = useAppDispatch()
-  const selectedJobId = useAppSelector(state => state.selectJob.job?.id)
+  const selectedJobId = useAppSelector(state => state.selectJob.job.id)
 
   return (
     <div className='flex flex-col gap-4 my-5'>
-      {
-        // CHANGE TYPE OF JOBITEM LATER
-        jobs &&
-          jobs.map((job: Job) => {
-            return (
-              <JobItem
-                key={job.id}
-                img={job.employer.avatar}
-                jobTitle={job.title}
-                companyName={job.employer.co_name}
-                city={job.city}
-                salary={job.salary}
-                timeDelta={job.timedelta}
-                onClick={() => {
-                  dispatch(setSelectedJob(job))
-                  dispatch(setIsSelected(true))
-                }}
-                className={selectedJobId === job.id && `border-primary-100`}
-              />
-            )
-          })
-      }
+      {jobs?.map((job: Job) => (
+        <JobItem
+          key={job.id}
+          img={job.employer.avatar}
+          jobTitle={job.title}
+          companyName={job.employer.co_name}
+          city={job.employer.city}
+          salary={job.salary}
+          timeDelta={job.timedelta}
+          onClick={() => {
+            dispatch(setSelectedJob(job))
+            dispatch(setIsSelected(true))
+          }}
+          className={selectedJobId === job.id && `border-primary-100`}
+        />
+      ))}
+      <h1>Pagination</h1>
     </div>
   )
 }
