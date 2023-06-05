14bd76dfb8334c346145fcff9881e32f7e2be437
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-05 18:00:51 +0330

    Refactor to use Axios

--- app/utils/fetchJobs.ts
+++ app/utils/fetchJobs.ts
@@ -1,17 +1,11 @@
-const fetchJobs = async (page: number, perPage: number) => {
-  const res = await fetch('http://199.231.235.83:8923/jobs/page', {
-    method: 'POST',
-    cache: 'no-store',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      page: page,
-      per_page: perPage
-    })
-  })
+import axios from 'axios'
 
-  return res.json()
+const fetchJobs = async (page: number, perPage: number) => {
+  const { data } = await axios.post('http://199.231.235.83:8923/jobs/page', {
+    page,
+    per_page: perPage
+  })
+  return data
 }
 
 export default fetchJobs
