ec1a41beafbdb5704312dcbb3736a387806ec88e
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-06 11:58:50 +0330

    Add rounded style

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -6,6 +6,7 @@ interface ButtonProps {
   secondary?: boolean
   success?: boolean
   outline?: boolean
+  rounded?: boolean
   type?: 'button' | 'submit'
   className?: string
   onClick?: React.MouseEventHandler<HTMLButtonElement>
@@ -17,16 +18,18 @@ const Button: React.FC<ButtonProps> = ({
   secondary,
   success,
   outline,
+  rounded,
   type,
   className,
   onClick
 }) => {
   const classes = classNames(
-    'px-6 py-2 border-[1.5px] rounded-lg hover:shadow-lg transition',
+    'px-6 py-2 border-[1.5px]  hover:shadow-lg transition',
     {
-      'border-primary-100 bg-primary-100 text-white hover:bg-primary-200 hover:border-primary-200': primary,
-      'border-success-100 bg-success-100 text-white hover:bg-success-200 hover:border-success-200': success,
-      'bg-transparent text-primary-100 border-primary-100': outline
+      'border-primary-100 bg-primary-100 text-white hover:bg-primary-200 hover:border-primary-200 rounded-lg': primary,
+      'border-success-100 bg-success-100 text-white hover:bg-success-200 hover:border-success-200 rounded-lg': success,
+      'bg-transparent text-primary-100 border-primary-100 rounded-lg': outline,
+      'rounded-full': rounded
     },
     className
   )
