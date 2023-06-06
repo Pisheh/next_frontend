845e23c9f81b44c777674fad1db9d3ac0e6e7bf2
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-06 11:56:52 +0330

    Initiate careers page

---
+++ app/careers/Hero.tsx
@@ -0,0 +1,15 @@
+import Card from '../components/Card'
+
+const Hero = () => {
+  return (
+    <section className='flex items-center justify-center w-full py-12 bg-primary-100'>
+      <Card className='h-[350px] rounded-3xl px-20'>
+        <h1 className='text-3xl font-extrabold'>
+          هدایت شغلی برای یافتن مسیر موفقیت
+        </h1>
+      </Card>
+    </section>
+  )
+}
+
+export default Hero
---
+++ app/careers/page.tsx
@@ -0,0 +1,11 @@
+import Hero from './Hero'
+
+const page = () => {
+  return (
+    <main className='mt-[84.75px]'>
+      <Hero />
+      <h1 className='text-4xl font-extrabold'>Careers Page</h1>
+    </main>
+  )
+}
+export default page
