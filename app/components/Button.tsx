2c305f3091c6c52e83adbe20533e0286c62e8c35
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-10 00:58:53 +0330

    Add icon button functionality

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,6 +7,7 @@ interface ButtonProps {
   success?: boolean
   outline?: boolean
   rounded?: boolean
+  icon?: boolean
   type?: 'button' | 'submit'
   className?: string
   onClick?: React.MouseEventHandler<HTMLButtonElement>
@@ -19,16 +20,17 @@ const Button: React.FC<ButtonProps> = ({
   success,
   outline,
   rounded,
+  icon,
   type,
   className,
   onClick
 }) => {
   const classes = classNames(
-    'px-6 py-2 border-[1.5px]  hover:shadow-lg transition',
+    'border-[1.5px]  hover:shadow-lg transition',
     {
-      'border-primary-100 bg-primary-100 text-white hover:bg-primary-200 hover:border-primary-200 rounded-lg': primary,
-      'border-success-100 bg-success-100 text-white hover:bg-success-200 hover:border-success-200 rounded-lg': success,
-      'bg-transparent text-primary-100 border-primary-100 rounded-lg': outline,
+      'border-primary-100 bg-primary-100 text-white hover:bg-primary-200 hover:border-primary-200 rounded-lg px-6 py-2': primary,
+      'border-success-100 bg-success-100 text-white hover:bg-success-200 hover:border-success-200 rounded-lg px-6 py-2': success,
+      'bg-transparent text-primary-100 border-primary-100 rounded-lg px-6 py-2': outline,
       'rounded-full': rounded
     },
     className
