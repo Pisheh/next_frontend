f15454770cf3f86ceba1f556f42d8f11e559b4ea
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-10 01:00:57 +0330

    Add support for smaller screens

--- app/jobs/JobDetails.tsx
+++ app/jobs/JobDetails.tsx
@@ -1,17 +1,16 @@
 'use client'
 
-import { useAppDispatch, useAppSelector } from '../redux/store/hooks'
+import { useAppSelector } from '../redux/store/hooks'
 import JobDetailHeader from './JobDetailHeader'
 import JobDetailBody from './JobDetailBody'
 
 const JobDetails = () => {
-  const dispatch = useAppDispatch()
   const { isSelected } = useAppSelector(state => state.selectJob)
 
   return (
     <>
       {isSelected && (
-        <div className='w-full my-5 bg-[#f9f9f9] rounded-2xl border shadow-sm sticky top-[100px] overflow-y-auto job-detail'>
+        <div className='w-full lg:my-5 bg-[#f9f9f9] lg:rounded-2xl border shadow-sm sticky top-[100px] overflow-y-auto job-detail'>
           <JobDetailHeader />
           <JobDetailBody />
         </div>
