8aeb4196a530569b295ef3d4ebb26aa538fe31b6
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-10 00:58:29 +0330

    Move Next Auth options to another file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,3 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-
-const GOOGLE_CLIENT_ID: any = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
-const GOOGLE_CLIENT_SECRET: any = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET
-    })
-  ]
-}
-
-const handler = NextAuth(authOptions)
+import handler from '@/app/lib/nextAuthOptions'
 
 export { handler as GET, handler as POST }
