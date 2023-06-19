975abb977fbdddc5ec4dbd0e7110bd1eb910b05e
Sepehr Gorgini <agent.1378@gmail.com>
2023-06-19 17:13:58 +0330

    Change fetch method to GET

--- app/utils/fetchJobs.ts
+++ app/utils/fetchJobs.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const fetchJobs = async (page: number, perPage: number) => {
-  const data = await axios.post('http://199.231.235.83:8923/jobs/page', {
-    page,
-    per_page: perPage
-  })
+  const data = await axios.get(
+    `http://199.231.235.83:8923/jobs?page=${page}&per_page=${perPage}`
+  )
   return data
 }
 
