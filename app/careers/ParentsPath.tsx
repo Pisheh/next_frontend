88cf9bd2ab7f1808537543d76f015fa5b56be20a
Sepehr Gorgini <agent.1378@gmail.com>
2023-07-09 12:13:43 +0400

    Update component

--- app/careers/ParentsPath.tsx
+++ app/careers/ParentsPath.tsx
@@ -1,12 +1,46 @@
-import { FormControl } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
+import { FormControl, FormLabel } from '@chakra-ui/react'
+import Button from '../components/Button'
+import SeekersInputFields from './SeekersInputFields'
+import Container from '../components/Container'
+import PersonalityMenu from './PersonalityMenu'
 
 const ParentsForm = () => {
   const { register, handleSubmit } = useForm()
+  const router = useRouter()
+
+  const handleClick = (e: any) => {
+    e.preventDefault()
+    router.push('/careers/guides')
+  }
+
   return (
-    <form>
-      <FormControl></FormControl>
-    </form>
+    <Container>
+      <div className='grid grid-cols-1 gap-16 mb-20 lg:grid-cols-2'>
+        <div>
+          <h2 className='mb-5 text-lg font-bold'>
+            تخصص متناسب با علایق فرزند خود را بیابید
+          </h2>
+          <form>
+            <FormControl>
+              <FormLabel>رشته</FormLabel>
+              <SeekersInputFields placeholder='رشته تحصیلی فرزند خود را انتخاب کنید' />
+              <FormLabel>تخصص</FormLabel>
+              <SeekersInputFields placeholder='تخصص مورد علاقه فرزند خود را انتخاب کنید' />
+              <PersonalityMenu />
+            </FormControl>
+            <Button
+              primary
+              onClick={e => handleClick(e)}
+              className='w-full lg:w-fit'
+            >
+              جستجو
+            </Button>
+          </form>
+        </div>
+      </div>
+    </Container>
   )
 }
 
